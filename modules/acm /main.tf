ccdcac1f966c041d4523722381977c51d3b14792
wcassw <wcassw@ymail.com>
2026-01-06 15:03:17 +0000

    Add ACM certificate and DNS validation resources

---
+++ modules/acm /main.tf
@@ -0,0 +1,32 @@
+# Request the certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create DNS validation records for each domain name
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in tolist(aws_acm_certificate.cert.domain_validation_options) :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+  
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}

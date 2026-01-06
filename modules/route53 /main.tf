5d48f4bc334676c274bcfe6758ae866147d22f0d
wcassw <wcassw@ymail.com>
2026-01-06 15:12:37 +0000

    Add Route 53 record resource configuration

---
+++ modules/route53 /main.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "app_alias" {
+  zone_id = var.hosted_zone_id
+  name    = var.record_name
+  type    = "A"
+
+  alias {
+    name                   = var.alb_dns_name
+    zone_id                = var.alb_zone_id
+    evaluate_target_health = true
+  }
+}

63fb8933a1b78900531d3b3e59b856c8d42568a4
wcassw <wcassw@ymail.com>
2026-01-06 15:13:38 +0000

    Add variables for Route 53 module

---
+++ modules/route53 /variables.tf
@@ -0,0 +1,13 @@
+variable "hosted_zone_id" { 
+    type = string 
+}
+variable "record_name"    { 
+    type = string 
+}
+
+variable "alb_dns_name" { 
+    type = string 
+}
+variable "alb_zone_id"  { 
+    type = string 
+}

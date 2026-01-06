48047fbe1ede238a2876c0a47103ac2d8efadd27
wcassw <wcassw@ymail.com>
2026-01-06 15:11:59 +0000

    Add variables for network module configuration

---
+++ modules/network /variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" { 
+    type = string 
+}
+variable "az1"        { 
+    type = string 
+}
+variable "az2"        { 
+    type = string 
+}
+
+variable "vpc_cidr" { 
+    type = string 
+}
+
+variable "public_subnet_az1_cidr" { 
+    type = string 
+}
+variable "public_subnet_az2_cidr" { 
+    type = string 
+}
+
+variable "private1_az1_cidr" { 
+    type = string 
+}
+variable "private2_az1_cidr" { 
+    type = string 
+}
+variable "private1_az2_cidr" { 
+    type = string 
+}
+variable "private2_az2_cidr" { 
+    type = string 
+}
